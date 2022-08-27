0e938e7dfb3fdcb51aa17ac7584ee36c027fbfe6
vinayak <itssvinayak@gmail.com>
2022-08-28 02:43:35 +0530

    todo with bind mount

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 sudo docker build -t nodejs_app . &&
-sudo docker run -p 3000:3000 -d -v todo:/app/todo --rm --name node_todo_app nodejs_app:latest
+sudo docker run -p 3000:3000 -d -v /home/vinayak/Documents/node_docker/todo:/app/todo --rm --name node_todo_app nodejs_app:latest
