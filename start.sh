de1dfac05e12cacf6e191cb5e7da02f2bc7ae4c0
vinayak <itssvinayak@gmail.com>
2022-08-28 02:47:59 +0530

    short path added

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 sudo docker build -t nodejs_app . &&
-sudo docker run -p 3000:3000 -d -v /home/vinayak/Documents/node_docker/todo:/app/todo --rm --name node_todo_app nodejs_app:latest
+# sudo docker run -p 3000:3000 -d -v /home/vinayak/Documents/node_docker/todo:/app/todo --rm --name node_todo_app nodejs_app:latest
+sudo docker run -p 3000:3000 -d -v $(pwd)/todo:/app/todo --rm --name node_todo_app nodejs_app:latest
