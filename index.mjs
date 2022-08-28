e66b0cc75f9b3ecd313c12fcc118422f59182a1f
vinayak <itssvinayak@gmail.com>
2022-08-28 06:38:07 +0530

    hot reload with mongo , connecting to dockers

--- index.mjs
+++ index.mjs
@@ -1,44 +1,33 @@
 import express from "express";
-import { promises as fs } from "fs";
-import path from "path";
+import mongoose from "mongoose";
+
+// setting up db schema
+
+const todoSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: true,
+  },
+});
+
+const todoModel = mongoose.model("todo", todoSchema);
 
 const app = new express();
 
-const __dirname = path.resolve();
-const dir = __dirname + "/todo";
-const fileExists = async (dir) => !!(await fs.stat(dir).catch((e) => false));
-
 app.use(express.urlencoded({ extended: false }));
 
-const todo = [];
-
-const enrichTodos = async () => {
-  let raw_todos;
-  try {
-    if (!fileExists(dir)) {
-      await fs.mkdir(dir);
-    }
-    raw_todos = await fs.readFile("./todo/todos.txt", "utf8");
-  } catch (err) {
-    console.error("unable to load todos ! ", err);
-  }
-  let todos = raw_todos.split("\n");
-  for (let item of todos) {
-    todo.push(item);
-  }
-};
-
-const printTodo = () => {
+const printTodo = async () => {
   let todo_html = [];
-  console.log(todo);
-  for (let i of todo) {
-    todo_html.push(`<li> ${i} </li>`);
+  let todos = await todoModel.find({});
+  todos = todos.map((todo) => todo.task);
+  for (let todo of todos) {
+    todo_html.push(`<li> ${todo} </li>`);
   }
-  let todos = todo_html.join(" ");
-  return todos;
+  let return_html = todo_html.join(" ");
+  return return_html;
 };
 
-app.get("/", (_req, res) => {
+app.get("/", async (_req, res) => {
   res.status(200).send(`
   <center>
     <h1> TODO </h1>
@@ -47,30 +36,37 @@ app.get("/", (_req, res) => {
       <input type="submit" value="Create">
     </form>
     <ol>
-      ${printTodo()}
+      ${await printTodo()}
     </ol>
-    <a href="/save"> SAVE TODOs </a>
   </center >
   `);
 });
 
-app.post("/create", (req, res) => {
-  todo.push(req.body.todo);
-  res.redirect("/");
-});
-
-app.get("/save", async (req, res) => {
-  let todos = todo.join("\n");
+app.post("/create", async (req, res) => {
   try {
-    await fs.appendFile("./todo/todos.txt", todos);
+    const todo = new todoModel({
+      task: req.body.todo,
+    });
+    await todo.save();
   } catch (err) {
-    console.error("unable to save todos ", err);
+    console.error("unable to save todo ", err);
   }
   res.redirect("/");
 });
 
+const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, async () => {
-  await enrichTodos();
-  console.log("Server started on port 3000");
-});
+
+console.log(DB_URL);
+mongoose
+  .connect(DB_URL)
+  .then((res) => {
+    console.log("connected to DB");
+    app.listen(PORT, () => {
+      console.log("Server started on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("unable to connect to DB ", err);
+    process.exit();
+  });
